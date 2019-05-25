23b901d190476bee7090331323c71cb481b29ad5
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-05-25 21:07:24 +0800

    Initial commit

---
+++ app/src/main/java/io/jmdg/android_service_demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.jmdg.android_service_demo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
