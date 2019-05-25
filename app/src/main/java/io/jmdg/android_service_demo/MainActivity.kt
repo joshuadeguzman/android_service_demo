ccaa5ded08a9ba7882a31e09794b3f1823f3a40e
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-05-25 21:23:52 +0800

    Initial commit

--- app/src/main/java/io/jmdg/android_service_demo/MainActivity.kt
+++ app/src/main/java/io/jmdg/android_service_demo/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.jmdg.android_service_demo
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Play local media
+        this.playLocalMediaSource()
+    }
+
+    private fun playLocalMediaSource() {
+        val mediaPlayer = MediaPlayer.create(applicationContext, R.raw.sample)
+        mediaPlayer?.start()
     }
 }
